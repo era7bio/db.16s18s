069681b6c24c1ea2d8425b272e1c90249133a751
Marina Manrique <mmanrique@era7.com>
2017-06-20 12:07:15 +0200

    fix link to reference db

--- src/test/scala/releaseData.scala
+++ src/test/scala/releaseData.scala
@@ -20,13 +20,13 @@ case object releaseData {
       .getOrElse(sys.error(s"Couldn't list objects in ${blastdbSource}"))
       .flatMap { obj =>
         obj.key.split('/').lastOption.map { name =>
-          obj -> (db.16s18s.data.blastDBS3 / name)
+          obj -> (db.both16s18s.data.blastDBS3 / name)
         }
       }
 
     copyData(transferManager)(
-      (dropInconsistentAssignments.output.fasta.s3 -> db.16s18s.data.fastaS3) ::
-      (dropInconsistentAssignments.output.table.s3 -> db.16s18s.data.id2taxasS3) ::
+      (dropInconsistentAssignments.output.fasta.s3 -> db.both16s18s.data.fastaS3) ::
+      (dropInconsistentAssignments.output.table.s3 -> db.both16s18s.data.id2taxasS3) ::
       blastdbMap
     )
 
